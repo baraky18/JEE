bfcfee03d71dde0059b09d4edc355942957c05fa
BARAKY <barak.yedaya@gmail.com>
2020-04-07 05:44:52 +0300

    first commit

---
+++ JSPs and Servlets/src/com/SimpleServlet.java
@@ -0,0 +1,25 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SimpleServlet
+ */
+@WebServlet(description = "Simple Servlet Description", urlPatterns = { "/SimpleServletPath" })
+public class SimpleServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from Servlet!");
+		response.getWriter().append("<h3>Hello in HTML</h3>");
+	}
+
+}
---
+++ JSPs and Servlets/src/com/XmlServlet.java
@@ -0,0 +1,39 @@
+package com;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class XmlServlet extends HttpServlet{
+
+	private static final long serialVersionUID = -3497925760066120950L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from XML GET Servlet!");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		out.println("Hello from GET " + userName);
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Hello from XML POST Servlet!");
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		String userName = request.getParameter("userName");
+		String fullName = request.getParameter("fullName");
+		String prof = request.getParameter("prof");
+		String gender = request.getParameter("gender");
+		String[] locations = request.getParameterValues("location");
+		out.println("Hello from POST " + userName + "! your full name is: " + fullName);
+		out.println("Your profession is " + prof);
+		out.println("Your gender is " + gender);
+		for(String location: locations){
+			out.println("Your location is " + location);
+		}
+	}
+}
